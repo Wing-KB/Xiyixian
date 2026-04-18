f32a484a4188c9a05fed4c6ec129de8a13140c4e
Wing-KB <xinchangyong@outlook.com>
2026-04-18 15:32:17 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,71 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # # #
+        . . . # #
+        . . # . #
+        . # . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . # . #
+        . . . # #
+        . . # # #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        # . # . .
+        # # . . .
+        # # # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Silly)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
