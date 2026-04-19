2227f0e5f644f8b060910acaa583bc9c1e7a937d
Wing-KB <xinchangyong@outlook.com>
2026-04-19 16:25:50 +0800

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,47 +1,58 @@
-def on_received_string(receivedString):
-    basic.show_string(receivedString)
-radio.on_received_string(on_received_string)
+def on_received_number(receivedNumber):
+    basic.show_leds("""
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        """)
+    basic.pause(500)
+    basic.clear_screen()
+radio.on_received_number(on_received_number)
+
+def on_button_pressed_a():
+    basic.show_leds("""
+        . . # . .
+        . # # # .
+        . # # # .
+        # # # # #
+        # # # # #
+        """)
+    basic.show_icon(IconNames.SILLY)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    basic.show_icon(IconNames.SWORD)
+    basic.show_icon(IconNames.SILLY)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    pass
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_sound_loud():
-    basic.show_leds("""
-        # . . . #
-        . # . # .
-        . . # . .
-        . # . # .
-        # . . . #
-        """)
+    basic.show_string("Hello!")
 input.on_sound(DetectedSound.LOUD, on_sound_loud)
-
-def Wait():
-    while True:
-        if input.temperature() <30:
-            return 0
 
 def on_sound_quiet():
     basic.show_leds("""
-        . . . . .
-        . . . . #
+        . # # # .
+        # . . . #
         . . . # .
-        # . # . .
-        . # . . .
+        . . # . .
+        . . # . .
         """)
 input.on_sound(DetectedSound.QUIET, on_sound_quiet)
 
-def on_forever():
-    if input.temperature() >= 30:
-        radio.set_group(1)
-        radio.send_string("hot")
-        if not (input.temperature() >= 30):
-            Wait()
-basic.forever(on_forever)
+basic.show_leds("""
+    . # # # .
+    # . . . #
+    . . . # .
+    . . # . .
+    . . # . .
+    """)
+basic.show_icon(IconNames.SWORD)
 
-def on_forever2():
-    if input.logo_is_pressed():
-        basic.show_leds("""
-            . . # . .
-            . # . # .
-            # . # . #
-            . # . # .
-            . . # . .
-            """)
-basic.forever(on_forever2)
+def on_forever():
+    radio.set_group(66)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,16 +1,68 @@
 radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . # . .
+            # # # # #
+            . # # # .
+            . . # . .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
+input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
+        . . # . .
+        . # # # .
+        . # # # .
         # # # # #
         # # # # #
         `)
-    basic.pause(500)
-    basic.clearScreen()
+    basic.showIcon(IconNames.Silly)
 })
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(53)
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Sword)
+    basic.showIcon(IconNames.Silly)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(0)
+})
+input.onSound(DetectedSound.Loud, function () {
+    while (true) {
+        basic.showString("Hello!")
+        if (input.soundLevel() <= 129) {
+            break;
+        }
+    }
+})
+input.onSound(DetectedSound.Quiet, function () {
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        . . . # .
+        . . # . .
+        . . # . .
+        `)
+})
+basic.showLeds(`
+    . # # # .
+    # . . . #
+    . . . # .
+    . . # . .
+    . . # . .
+    `)
+basic.showIcon(IconNames.Sword)
+basic.forever(function () {
+    if (input.logoIsPressed()) {
+        radio.sendNumber(1)
+    }
 })
 basic.forever(function () {
     radio.setGroup(66)
